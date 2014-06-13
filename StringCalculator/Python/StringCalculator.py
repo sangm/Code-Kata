a5a7b671971ca9a69119fc89b4642d36f0d8e192
sangm <sang.mercado@gmail.com>
2014-06-12 21:50:56 -0500

    (Python) Finishing StringCalculator

--- StringCalculator/Python/StringCalculator.py
+++ StringCalculator/Python/StringCalculator.py
@@ -1,18 +1,28 @@
+import string
 import re
 
 class StringCalculator:
+    
     def add(self, numbers):
         if len(numbers) == 0: return 0
-        regex = re.compile(r'^(//?).\\n')
-        search = regex.search(numbers)
-
+        regex = re.compile(r'(//?).+\\n').search(numbers)
         delim = ','
-        if search:
-            index = search.end()
-            numbers = numbers[index:]
-            delim = search.group().replace(r'\n','').replace(r'//','')
-
-        numbers = numbers.replace('\n', ',').split(delim)
-        sumNum = sum([int(num) for num in numbers])
-        return sumNum
-
+        if regex:
+            delim = regex.group().replace('//','').replace(r'\n','')
+            numbers = numbers[regex.end():]
+        regex = re.compile(r'\[(.+?)\]')
+        search = regex.match(delim)
+        if search: 
+            delimList = []
+            while search:
+                delimList.append(search.group(1))
+                delim = delim[search.end():]
+                search = regex.match(delim)
+            numbers = re.sub('[%s]' % ''.join(delimList), delimList[0], numbers)
+            delim = delimList[0]
+        numbers = numbers.replace(r'\n', delim)
+        numbers = numbers.split(delim)
+        numbers = [int(n) for n in numbers if int(n) < 1000]
+        if any(n < 0 for n in numbers):
+            raise Exception('Negative Numbers Not Allowed')
+        return sum(numbers)
--- StringCalculator/Python/TestStringCalculator.py
+++ StringCalculator/Python/TestStringCalculator.py
@@ -6,61 +6,62 @@ import re
 class TestStringCalculator(unittest.TestCase):
     
     def setUp(self):
-        self.stringCalc = StringCalculator.StringCalculator()
+        self.calc = StringCalculator.StringCalculator()
 
-    def test_empty_string(self):
-        self.assertEqual(self.stringCalc.add(""), 0)
+    def test_empy_string(self):
+        self.assertEqual(self.calc.add(""), 0)
     def test_one_string(self):
-        self.assertEqual(self.stringCalc.add("1"), 1)
-        self.assertEqual(self.stringCalc.add("2"), 2)
+        self.assertEqual(self.calc.add("1"), 1)
     def test_two_strings(self):
-        self.assertEqual(self.stringCalc.add("1,2"), 3)
-        self.assertEqual(self.stringCalc.add("5,6"), 11)
-    def test_one_random_string(self):
-        randomNum = random.randint(1, 10000)
-        self.assertEqual(self.stringCalc.add(str(randomNum)), randomNum)
-    def test_two_random_strings(self):
-        randomNumOne = random.randint(1, 100)
-        randomNumTwo = random.randint(2, 10000)
-        inputNums = ','.join([str(randomNumOne), str(randomNumTwo)])
-        self.assertTrue(isinstance(inputNums,str))
-        self.assertEqual(self.stringCalc.add(inputNums), randomNumOne+randomNumTwo)
+        self.assertEqual(self.calc.add("1,2"), 3)
+        self.assertEqual(self.calc.add("3,4"), 7)
     def test_three_strings(self):
-        self.assertEqual(self.stringCalc.add('1,2,3'), 6)
-        self.assertEqual(self.stringCalc.add('3,5,6,7'), 21)
-    def test_random_number_of_strings(self):
-        randomNum = random.randint(1, 100)
-        randomList = random.sample(range(1000), randomNum)
-        sumNum = sum(randomList)
-        inputNums = ','.join([str(x) for x in randomList])
-        self.assertEqual(self.stringCalc.add(inputNums), sumNum)
-    def test_newline_character(self):
-        inputNums = '1\n2,3'
-        self.assertEqual(self.stringCalc.add(inputNums), 6)
+        self.assertEqual(self.calc.add("1,2,3"), 6)
+    def test_random_number_strings(self):
+        randNum = random.randint(1, 1000)
+        randomNumbers = random.sample(range(1, 1000), randNum)
+        randomString = ','.join([str(n) for n in randomNumbers])
+        self.assertEqual(self.calc.add(randomString), sum(randomNumbers))
+    def test_handle_new_line(self):
+        self.assertEqual(self.calc.add(r'1\n2,3'), 6)
     def test_regex_api(self):
-        testString1 = 'abc'
         simple = re.compile('abc')
-        self.assertEqual(simple.search(testString1).group(), testString1)
+        testString = r'abc'
+        testString2 = r'aaaabc'
+        testString3 = r'//?\n'
 
-        testString = r'//;\n1;2'
+        # match returns something only if the string BEGINS with the regex
+        self.assertIsNotNone(simple.match(testString))
+        self.assertIsNone(simple.match(testString2))
+        self.assertEqual(simple.match(testString).group(), 'abc')
+        self.assertEqual(simple.search(testString2).group(), 'abc')
+
         regex = re.compile(r'^(//?).\\n')
-        self.assertEqual(regex.search(testString).group(), r'//;\n')
-
-        index = regex.search(testString).end()
-        self.assertEqual(testString[index:], '1;2')
-    def test_default_delimiter(self):
-        inputString = r'//?\n1?2?3'
-        self.assertEqual(self.stringCalc.add(inputString), 6)
-
-
-
-
-    
-
-
-
-
-
+        regex = regex.match(testString3)
+        self.assertIsNotNone(regex)
+        self.assertEqual(regex.group().replace('//','').replace(r'\n',''), '?')
+        self.assertEqual(regex.end(), 5)
+    def test_different_delimiter(self):
+        self.assertEqual(self.calc.add(r'//x\n1x2x3x4'), 10)
+    def test_negative_number_raises_exception(self):
+        self.assertRaises(Exception, self.calc.add, '-1,2')
+    def test_numbers_above_1000_ignored(self):
+        self.assertEqual(self.calc.add('1,2,3000'), 3)
+    def test_regex_api_long_delim(self):
+        inputStr = r'[%][$][%%]'
+        regex = re.compile(r'\[(.+?)\]')
+        delim = []
+        while inputStr:
+            search = regex.search(inputStr)
+            delim.append(search.group(1))
+            inputStr = inputStr[search.end():]
+        self.assertEqual(delim, ['%','$','%%'])
+    def test_long_delimiter(self):
+        inputString = r'//***\n1***2***3'
+        self.assertEqual(self.calc.add(inputString), 6)
+    def test_multi_delimiter(self):
+        inputString = r'//[%][*]\n1%2*2'
+        self.assertEqual(self.calc.add(inputString), 5)
 
 if __name__ == '__main__':
     unittest.main()
